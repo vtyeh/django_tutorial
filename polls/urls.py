b3ab59f97979e45c1664c94492dd44296da1dab7
Venessa Yeh <vtyeh@uci.edu>
2019-11-09 16:18:57 -0800

    Swapped out hardcode for generic views.

--- polls/urls.py
+++ polls/urls.py
@@ -7,11 +7,11 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     # /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # /polls/2
-    path('specifics/<int:question_id>/', views.detail, name='detail'),
+    path('specifics/<int:pk>/', views.DetailView.as_view(), name='detail'),
     # /polls/2/results
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # /polls/2/vote
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -10,36 +10,29 @@ In Django, web pages and other content are delivered by views. Each view is repr
 simple Python function (or method, in the case of class-based views). Django will choose a view 
 by examining the URL that’s requested (to be precise, the part of the URL after the domain name).
 """
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.http import HttpResponseRedirect #HttpResponse, Http404
 # from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Question, Choice
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # output = ', '.join([q.question_text for q in latest_question_list]) --> return HttpResponse(output)
-    # template = loader.get_template('polls/index.html') --> return HttpResponse(template.render(context, request))
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-    # This line is better than above because it promotes loose coupling
-    # The above couples view layer with model layer
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -54,9 +47,7 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-    # Always return an HttpResponseRedirect after successfully dealing
-    # with POST data. This prevents data from being posted twice if a
-    # user hits the Back button.
+    # Always return an HttpResponseRedirect after successfully dealing with POST data. 
+    # This prevents data from being posted twice if a user hits the Back button.
     # reverse() helps to avoid having to hardcode a URL in the view function
     return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
